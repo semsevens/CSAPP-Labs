c0c3d12cd51d5e8f82f63bbe3a8b87e64a27ca69
Soros Liu <soros.liu1029@gmail.com>
2020-09-06 15:37:48 +0800

    refactor: improved version for cache lab part B test 1

--- cache-lab/trans.c
+++ cache-lab/trans.c
@@ -15,52 +15,9 @@
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
 void swap_matrix_element(int *a, int *b) {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
-void transpose_square_matrix(int N, int A[N][N], int B[N][N]) {
-
-    int a0, a1, a2, a3, a4, a5, a6, a7;
-    size_t ii, jj, i, j;
-    for (ii = 0; ii < N; ii += BSIZE) {
-        for (jj = ii; jj < N; jj += BSIZE) {
-            // copy from A
-            for (i = ii; i < ii + 8; i++) {
-                j = jj;
-                a0 = A[i][j], a1 = A[i][j + 1], a2 = A[i][j + 2], a3 = A[i][j + 3], a4 = A[i][j + 4], a5 = A[i][j + 5], a6 = A[i][j + 6], a7 = A[i][j + 7];
-                B[i][j] = a0, B[i][j + 1] = a1, B[i][j + 2] = a2, B[i][j + 3] = a3, B[i][j + 4] = a4, B[i][j + 5] = a5, B[i][j + 6] = a6, B[i][j + 7] = a7;
-            }
-            if (ii != jj) {
-                for (i = jj; i < jj + 8; i++) {
-                    j = ii;
-                    a0 = A[i][j], a1 = A[i][j + 1], a2 = A[i][j + 2], a3 = A[i][j + 3], a4 = A[i][j + 4], a5 = A[i][j + 5], a6 = A[i][j + 6], a7 = A[i][j + 7];
-                    B[i][j] = a0, B[i][j + 1] = a1, B[i][j + 2] = a2, B[i][j + 3] = a3, B[i][j + 4] = a4, B[i][j + 5] = a5, B[i][j + 6] = a6, B[i][j + 7] = a7;
-                }
-            }
-
-            for (i = ii; i < ii + BSIZE; i++) {
-                for (j = ii == jj ? i + 1 : jj; j < jj + BSIZE; j++) {
-                    swap_matrix_element(&B[i][j], &B[j][i]);
-                }
-            }
-        }
-    }
-}
-
-/* 
- * transpose_submit - This is the solution transpose function that you
- *     will be graded on for Part B of the assignment. Do not change
- *     the description string "Transpose submission", as the driver
- *     searches for that string to identify the transpose function to
- *     be graded. 
- */
-char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
-    if (M == N) {
-        transpose_square_matrix(N, A, B);
-    }
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
 }
 
 /* 
@@ -95,6 +52,74 @@ void trans_column_wise(int M, int N, int A[N][M], int B[M][N]) {
     }
 }
 
+char trans_32_square_matrix_desc[] = "Block wise transpose, especially for 32*32 matrix";
+void transpose_32_square_matrix(int M, int N, int A[N][M], int B[M][N]) {
+    if (M != N || N != 32) {
+        return;
+    }
+
+    int a0, a1, a2, a3, a4, a5, a6, a7;
+    size_t ii, jj, i, j;
+    for (ii = 0; ii < N; ii += BSIZE) {
+        for (jj = ii; jj < N; jj += BSIZE) {
+            // copy from A
+            for (i = ii; i < ii + 8; i++) {
+                j = jj;
+                a0 = A[i][j], a1 = A[i][j + 1], a2 = A[i][j + 2], a3 = A[i][j + 3], a4 = A[i][j + 4], a5 = A[i][j + 5], a6 = A[i][j + 6], a7 = A[i][j + 7];
+                B[i][j] = a0, B[i][j + 1] = a1, B[i][j + 2] = a2, B[i][j + 3] = a3, B[i][j + 4] = a4, B[i][j + 5] = a5, B[i][j + 6] = a6, B[i][j + 7] = a7;
+            }
+            if (ii != jj) {
+                for (i = jj; i < jj + 8; i++) {
+                    j = ii;
+                    a0 = A[i][j], a1 = A[i][j + 1], a2 = A[i][j + 2], a3 = A[i][j + 3], a4 = A[i][j + 4], a5 = A[i][j + 5], a6 = A[i][j + 6], a7 = A[i][j + 7];
+                    B[i][j] = a0, B[i][j + 1] = a1, B[i][j + 2] = a2, B[i][j + 3] = a3, B[i][j + 4] = a4, B[i][j + 5] = a5, B[i][j + 6] = a6, B[i][j + 7] = a7;
+                }
+            }
+
+            // use block, which `bsize=8`, to transpose
+            for (i = ii; i < ii + BSIZE; i++) {
+                for (j = ii == jj ? i + 1 : jj; j < jj + BSIZE; j++) {
+                    swap_matrix_element(&B[i][j], &B[j][i]);
+                }
+            }
+        }
+    }
+}
+
+
+char trans_32_square_matrix_v2_desc[] = "Block wise transpose version 2, especially for 32*32 matrix";
+void transpose_32_square_matrix_v2(int M, int N, int A[N][M], int B[M][N]) {
+    if (M != N || N != 32) {
+        return;
+    }
+
+    int a0, a1, a2, a3, a4, a5, a6, a7;
+    size_t ii, jj, i, j;
+    for (ii = 0; ii < N; ii += BSIZE) {
+        for (jj = 0; jj < N; jj += BSIZE) {
+            for (i = ii; i < ii + 8; i++) {
+                j = jj;
+                a0 = A[i][j], a1 = A[i][j + 1], a2 = A[i][j + 2], a3 = A[i][j + 3], a4 = A[i][j + 4], a5 = A[i][j + 5], a6 = A[i][j + 6], a7 = A[i][j + 7];
+                B[j][i] = a0, B[j + 1][i] = a1, B[j + 2][i] = a2, B[j + 3][i] = a3, B[j + 4][i] = a4, B[j + 5][i] = a5, B[j + 6][i] = a6, B[j + 7][i] = a7;
+            }
+        }
+    }
+}
+
+/* 
+ * transpose_submit - This is the solution transpose function that you
+ *     will be graded on for Part B of the assignment. Do not change
+ *     the description string "Transpose submission", as the driver
+ *     searches for that string to identify the transpose function to
+ *     be graded. 
+ */
+char transpose_submit_desc[] = "Transpose submission";
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
+    if (M == N && N == 32) {
+        transpose_32_square_matrix_v2(M, N, A, B);
+    }
+}
+
 /*
  * registerFunctions - This function registers your transpose
  *     functions with the driver.  At runtime, the driver will
@@ -109,6 +134,8 @@ void registerFunctions() {
     /* Register any additional transpose functions */
     // registerTransFunction(trans_row_wise, trans_row_wise_desc);
     // registerTransFunction(trans_column_wise, trans_column_wise_desc);
+    // registerTransFunction(transpose_32_square_matrix, trans_32_square_matrix_desc);
+    // registerTransFunction(transpose_32_square_matrix_v2, trans_32_square_matrix_v2_desc);
 }
 
 /* 
