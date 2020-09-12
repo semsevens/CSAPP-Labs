338cabcc0f256a3ff77c8939b371a80c74b30919
Soros Liu <soros.liu1029@gmail.com>
2020-09-12 14:55:29 +0800

    feat: add some try to cache lab part B test 2

--- cache-lab/parse-hit-miss.c
+++ cache-lab/parse-hit-miss.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -42,14 +43,18 @@ int main(int argc, char *argv[]) {
         line[strcspn(line, "\n")] = '\0';
         address = strtoul(line + 2, NULL, 16);
 
+        printf("%s ", line);
+
         if (address >= A && address < A + A_row_size * N) {
-            i = address / A_row_size;
-            j = (address % A_row_size) / sizeof(int);
-            printf("%s A[%d][%d]\n", line, i, j);
+            i = (address - A) / A_row_size;
+            j = ((address - A) % A_row_size) / sizeof(int);
+            printf("A[%d][%d]", i, j);
         } else if (address >= B && address < B + B_row_size * M) {
-            i = address / B_row_size;
-            j = (address % B_row_size) / sizeof(int);
-            printf("%s B[%d][%d]\n", line, i, j);
+            i = (address - B) / B_row_size;
+            j = ((address - B) % B_row_size) / sizeof(int);
+            printf("B[%d][%d]", i, j);
         }
+
+        printf("\n");
     }
 }
--- cache-lab/trans.c
+++ cache-lab/trans.c
@@ -10,14 +10,36 @@
 #include "cachelab.h"
 #include <stdio.h>
 
-#define BSIZE 8
-
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
 void swap_matrix_element(int *a, int *b) {
     *a = *a + *b;
     *b = *a - *b;
     *a = *a - *b;
+}
+
+void trans_bsize_8(int i, int j, int M, int N, int A[N][M], int B[M][N]) {
+    int a0 = A[i][j], a1 = A[i][j + 1], a2 = A[i][j + 2], a3 = A[i][j + 3], a4 = A[i][j + 4], a5 = A[i][j + 5], a6 = A[i][j + 6], a7 = A[i][j + 7];
+    B[j][i] = a0, B[j + 1][i] = a1, B[j + 2][i] = a2, B[j + 3][i] = a3, B[j + 4][i] = a4, B[j + 5][i] = a5, B[j + 6][i] = a6, B[j + 7][i] = a7;
+}
+
+void trans_bsize_4(int i, int j, int M, int N, int A[N][M], int B[M][N]) {
+    int a0 = A[i][j], a1 = A[i][j + 1], a2 = A[i][j + 2], a3 = A[i][j + 3];
+    B[j][i] = a0, B[j + 1][i] = a1, B[j + 2][i] = a2, B[j + 3][i] = a3;
+}
+
+void copy_bsize_8(int i, int j, int M, int N, int A[N][M], int B[M][N]) {
+    int a0 = A[i][j], a1 = A[i][j + 1], a2 = A[i][j + 2], a3 = A[i][j + 3], a4 = A[i][j + 4], a5 = A[i][j + 5], a6 = A[i][j + 6], a7 = A[i][j + 7];
+    B[i][j] = a0, B[i][j + 1] = a1, B[i][j + 2] = a2, B[i][j + 3] = a3, B[i][j + 4] = a4, B[i][j + 5] = a5, B[i][j + 6] = a6, B[i][j + 7] = a7;
+}
+
+void trans_bsize_2x4(int i, int j, int M, int N, int A[N][M], int B[M][N]) {
+    int a00 = A[i][j], a01 = A[i][j + 1], a02 = A[i][j + 2], a03 = A[i][j + 3],
+        a10 = A[i + 1][j], a11 = A[i + 1][j + 1], a12 = A[i + 1][j + 2], a13 = A[i + 1][j + 3];
+    B[j][i] = a00, B[j][i + 1] = a10,
+    B[j + 1][i] = a01, B[j + 1][i + 1] = a11,
+    B[j + 2][i] = a02, B[j + 2][i + 1] = a12,
+    B[j + 3][i] = a03, B[j + 3][i + 1] = a13;
 }
 
 /* 
@@ -52,27 +74,26 @@ void trans_column_wise(int M, int N, int A[N][M], int B[M][N]) {
     }
 }
 
-char trans_32_square_matrix_desc[] = "Block wise transpose, especially for 32*32 matrix";
+char trans_32_square_matrix_desc[] = "Block wise transpose, only for 32*32 matrix";
 void transpose_32_square_matrix(int M, int N, int A[N][M], int B[M][N]) {
     if (M != N || N != 32) {
         return;
     }
 
-    int a0, a1, a2, a3, a4, a5, a6, a7;
+    const int BSIZE = 8;
+
     size_t ii, jj, i, j;
     for (ii = 0; ii < N; ii += BSIZE) {
         for (jj = ii; jj < N; jj += BSIZE) {
             // copy from A
             for (i = ii; i < ii + 8; i++) {
                 j = jj;
-                a0 = A[i][j], a1 = A[i][j + 1], a2 = A[i][j + 2], a3 = A[i][j + 3], a4 = A[i][j + 4], a5 = A[i][j + 5], a6 = A[i][j + 6], a7 = A[i][j + 7];
-                B[i][j] = a0, B[i][j + 1] = a1, B[i][j + 2] = a2, B[i][j + 3] = a3, B[i][j + 4] = a4, B[i][j + 5] = a5, B[i][j + 6] = a6, B[i][j + 7] = a7;
+                copy_bsize_8(i, j, M, N, A, B);
             }
             if (ii != jj) {
                 for (i = jj; i < jj + 8; i++) {
                     j = ii;
-                    a0 = A[i][j], a1 = A[i][j + 1], a2 = A[i][j + 2], a3 = A[i][j + 3], a4 = A[i][j + 4], a5 = A[i][j + 5], a6 = A[i][j + 6], a7 = A[i][j + 7];
-                    B[i][j] = a0, B[i][j + 1] = a1, B[i][j + 2] = a2, B[i][j + 3] = a3, B[i][j + 4] = a4, B[i][j + 5] = a5, B[i][j + 6] = a6, B[i][j + 7] = a7;
+                    copy_bsize_8(i, j, M, N, A, B);
                 }
             }
 
@@ -86,21 +107,286 @@ void transpose_32_square_matrix(int M, int N, int A[N][M], int B[M][N]) {
     }
 }
 
-
-char trans_32_square_matrix_v2_desc[] = "Block wise transpose version 2, especially for 32*32 matrix";
+char trans_32_square_matrix_v2_desc[] = "Block wise transpose version 2, only for 32*32 matrix";
 void transpose_32_square_matrix_v2(int M, int N, int A[N][M], int B[M][N]) {
     if (M != N || N != 32) {
         return;
     }
 
-    int a0, a1, a2, a3, a4, a5, a6, a7;
-    size_t ii, jj, i, j;
-    for (ii = 0; ii < N; ii += BSIZE) {
-        for (jj = 0; jj < N; jj += BSIZE) {
-            for (i = ii; i < ii + 8; i++) {
-                j = jj;
-                a0 = A[i][j], a1 = A[i][j + 1], a2 = A[i][j + 2], a3 = A[i][j + 3], a4 = A[i][j + 4], a5 = A[i][j + 5], a6 = A[i][j + 6], a7 = A[i][j + 7];
-                B[j][i] = a0, B[j + 1][i] = a1, B[j + 2][i] = a2, B[j + 3][i] = a3, B[j + 4][i] = a4, B[j + 5][i] = a5, B[j + 6][i] = a6, B[j + 7][i] = a7;
+    const int BSIZE = 8;
+
+    // block index
+    size_t bi, bj;
+    // inner-block index
+    size_t i, j;
+
+    for (bi = 0; bi < N; bi += BSIZE) {
+        for (bj = 0; bj < N; bj += BSIZE) {
+            for (i = bi; i < bi + BSIZE; i++) {
+                j = bj;
+                trans_bsize_8(i, j, M, N, A, B);
+            }
+        }
+    }
+}
+
+char trans_64_square_matrix_desc[] = "Block wise transpose, with zig-zag access pattern, only for 64*64 matrix";
+void transpose_64_square_matrix(int M, int N, int A[N][M], int B[M][N]) {
+    if (M != N || N != 64) {
+        return;
+    }
+
+    const int BSIZE = 4;
+
+    // zig-zag index
+    size_t zzi, zzj;
+    // block count in a zig-zag pattern
+    int zbc;
+    // block index in a zig-zag pattern
+    size_t bi;
+    // block upper left index (i, j)
+    size_t bul_i, bul_j;
+    // inner-block index
+    size_t i, j;
+
+    for (zzi = 0, zbc = 1; zzi < N; zzi += BSIZE, zbc++) {
+        for (bi = 0; bi < zbc; bi++) {
+            bul_i = zzi - bi * BSIZE, bul_j = 0 + bi * BSIZE;
+
+            for (i = bul_i; i < bul_i + BSIZE; i++) {
+                j = bul_j;
+                trans_bsize_4(i, j, M, N, A, B);
+            }
+        }
+    }
+
+    // cross anti-diagonal, so zig-zag block count will decrease one by one
+    zbc--;
+
+    for (zzj = BSIZE; zzj < N; zzj += BSIZE, zbc--) {
+        for (bi = 0; bi < zbc; bi++) {
+            bul_i = N - (bi + 1) * BSIZE, bul_j = zzj + bi * BSIZE;
+
+            for (i = bul_i; i < bul_i + BSIZE; i++) {
+                j = bul_j;
+                trans_bsize_4(i, j, M, N, A, B);
+            }
+        }
+    }
+}
+
+char trans_64_square_matrix_v2_desc[] = "Block wise transpose, with zig-zag access pattern, with continuous access style, only for 64*64 matrix";
+void transpose_64_square_matrix_v2(int M, int N, int A[N][M], int B[M][N]) {
+    if (M != N || N != 64) {
+        return;
+    }
+
+    const int BSIZE = 4;
+
+    // block upper left index (i, j)
+    size_t bul_i = 0, bul_j = 0;
+    // the stride to move to next block
+    size_t delta_i, delta_j;
+    // inner-block index
+    size_t i, j;
+
+    while (bul_i < N && bul_j < N) {
+        for (i = bul_i; i < bul_i + BSIZE; i++) {
+            j = bul_j;
+            trans_bsize_4(i, j, M, N, A, B);
+        }
+
+        if ((bul_j / 4) % 2 == 0) {
+            if (bul_i == 0) {
+                bul_j += BSIZE;
+                delta_i = BSIZE;
+                delta_j = -BSIZE;
+                continue;
+            } else if (bul_i == N - BSIZE) {
+                bul_j += BSIZE;
+                delta_i = -BSIZE;
+                delta_j = BSIZE;
+                continue;
+            }
+        }
+        if ((bul_i / 4) % 2 == 1) {
+            if (bul_j == 0) {
+                bul_i += BSIZE;
+                delta_i = -BSIZE;
+                delta_j = BSIZE;
+                continue;
+            } else if (bul_j == N - BSIZE) {
+                bul_i += BSIZE;
+                delta_i = BSIZE;
+                delta_j = -BSIZE;
+                continue;
+            }
+        }
+
+        bul_i += delta_i;
+        bul_j += delta_j;
+    }
+}
+
+char trans_64_square_matrix_v3_desc[] = "Block wise transpose, with inner block clock-wise access pattern, only for 64*64 matrix";
+void transpose_64_square_matrix_v3(int M, int N, int A[N][M], int B[M][N]) {
+    if (M != N || N != 64) {
+        return;
+    }
+
+    const int BSIZE = 8;
+    const int INNER_BSIZE = 4;
+    size_t bi, bj;
+    size_t i, j;
+
+    for (bi = 0; bi < N; bi += BSIZE) {
+        for (bj = 0; bj < N; bj += BSIZE) {
+
+            // inner upper left
+            for (i = bi; i < bi + INNER_BSIZE; i++) {
+                j = bj;
+                trans_bsize_4(i, j, M, N, A, B);
+            }
+
+            // inner upper right
+            for (i = bi; i < bi + INNER_BSIZE; i++) {
+                j = bj + INNER_BSIZE;
+                trans_bsize_4(i, j, M, N, A, B);
+            }
+
+            // inner bottom right
+            for (i = bi + INNER_BSIZE; i < bi + BSIZE; i++) {
+                j = bj + INNER_BSIZE;
+                trans_bsize_4(i, j, M, N, A, B);
+            }
+
+            // inner bottom left
+            for (i = bi + INNER_BSIZE; i < bi + BSIZE; i++) {
+                j = bj;
+                trans_bsize_4(i, j, M, N, A, B);
+            }
+        }
+    }
+}
+
+char trans_64_square_matrix_v4_desc[] = "Block wise transpose, with inner block counter-clock-wise access pattern, only for 64*64 matrix";
+void transpose_64_square_matrix_v4(int M, int N, int A[N][M], int B[M][N]) {
+    if (M != N || N != 64) {
+        return;
+    }
+
+    const int BSIZE = 8;
+    const int INNER_BSIZE = 4;
+    size_t bi, bj;
+    size_t i, j;
+
+    for (bi = 0; bi < N; bi += BSIZE) {
+        for (bj = 0; bj < N; bj += BSIZE) {
+
+            // inner upper left
+            for (i = bi; i < bi + INNER_BSIZE; i++) {
+                j = bj;
+                trans_bsize_4(i, j, M, N, A, B);
+            }
+
+            // inner bottom left
+            for (i = bi + INNER_BSIZE; i < bi + BSIZE; i++) {
+                j = bj;
+                trans_bsize_4(i, j, M, N, A, B);
+            }
+
+            // inner bottom right
+            for (i = bi + INNER_BSIZE; i < bi + BSIZE; i++) {
+                j = bj + INNER_BSIZE;
+                trans_bsize_4(i, j, M, N, A, B);
+            }
+
+            // inner upper right
+            for (i = bi; i < bi + INNER_BSIZE; i++) {
+                j = bj + INNER_BSIZE;
+                trans_bsize_4(i, j, M, N, A, B);
+            }
+        }
+    }
+}
+
+char trans_64_square_matrix_v5_desc[] = "Block wise transpose, with inner block clock-wise access pattern and 2*4 inner blocksize, only for 64*64 matrix";
+void transpose_64_square_matrix_v5(int M, int N, int A[N][M], int B[M][N]) {
+    if (M != N || N != 64) {
+        return;
+    }
+
+    const int BSIZE = 8;
+    const int INNER_BSIZE = 4;
+    size_t bi, bj;
+    size_t i, j;
+
+    for (bi = 0; bi < N; bi += BSIZE) {
+        for (bj = 0; bj < N; bj += BSIZE) {
+
+            // inner upper left
+            for (i = bi; i < bi + INNER_BSIZE; i += 2) {
+                j = bj;
+                trans_bsize_2x4(i, j, M, N, A, B);
+            }
+
+            // inner upper right
+            for (i = bi; i < bi + INNER_BSIZE; i += 2) {
+                j = bj + INNER_BSIZE;
+                trans_bsize_2x4(i, j, M, N, A, B);
+            }
+
+            // inner bottom right
+            for (i = bi + INNER_BSIZE; i < bi + BSIZE; i += 2) {
+                j = bj + INNER_BSIZE;
+                trans_bsize_2x4(i, j, M, N, A, B);
+            }
+
+            // inner bottom left
+            for (i = bi + INNER_BSIZE; i < bi + BSIZE; i += 2) {
+                j = bj;
+                trans_bsize_2x4(i, j, M, N, A, B);
+            }
+        }
+    }
+}
+
+char trans_64_square_matrix_v6_desc[] = "Block wise transpose, with inner block counter-clock-wise access pattern and 2*4 inner blocksize, only for 64*64 matrix";
+void transpose_64_square_matrix_v6(int M, int N, int A[N][M], int B[M][N]) {
+    if (M != N || N != 64) {
+        return;
+    }
+
+    const int BSIZE = 8;
+    const int INNER_BSIZE = 4;
+    size_t bi, bj;
+    size_t i, j;
+
+    for (bi = 0; bi < N; bi += BSIZE) {
+        for (bj = 0; bj < N; bj += BSIZE) {
+
+            // inner upper left
+            for (i = bi; i < bi + INNER_BSIZE; i += 2) {
+                j = bj;
+                trans_bsize_2x4(i, j, M, N, A, B);
+            }
+
+            // inner bottom left
+            for (i = bi + INNER_BSIZE; i < bi + BSIZE; i += 2) {
+                j = bj;
+                trans_bsize_2x4(i, j, M, N, A, B);
+            }
+
+            // inner bottom right
+            for (i = bi + INNER_BSIZE; i < bi + BSIZE; i += 2) {
+                j = bj + INNER_BSIZE;
+                trans_bsize_2x4(i, j, M, N, A, B);
+            }
+
+            // inner upper right
+            for (i = bi; i < bi + INNER_BSIZE; i += 2) {
+                j = bj + INNER_BSIZE;
+                trans_bsize_2x4(i, j, M, N, A, B);
             }
         }
     }
@@ -115,8 +401,12 @@ void transpose_32_square_matrix_v2(int M, int N, int A[N][M], int B[M][N]) {
  */
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
-    if (M == N && N == 32) {
+    if (M == 32 && N == 32) {
         transpose_32_square_matrix_v2(M, N, A, B);
+    } else if (M == 64 && N == 64) {
+        transpose_64_square_matrix_v3(M, N, A, B);
+    } else {
+        trans_row_wise(M, N, A, B);
     }
 }
 
@@ -134,8 +424,16 @@ void registerFunctions() {
     /* Register any additional transpose functions */
     // registerTransFunction(trans_row_wise, trans_row_wise_desc);
     // registerTransFunction(trans_column_wise, trans_column_wise_desc);
+
     // registerTransFunction(transpose_32_square_matrix, trans_32_square_matrix_desc);
     // registerTransFunction(transpose_32_square_matrix_v2, trans_32_square_matrix_v2_desc);
+
+    // registerTransFunction(transpose_64_square_matrix, trans_64_square_matrix_desc);
+    // registerTransFunction(transpose_64_square_matrix_v2, trans_64_square_matrix_v2_desc);
+    // registerTransFunction(transpose_64_square_matrix_v3, trans_64_square_matrix_v3_desc);
+    // registerTransFunction(transpose_64_square_matrix_v4, trans_64_square_matrix_v4_desc);
+    // registerTransFunction(transpose_64_square_matrix_v5, trans_64_square_matrix_v5_desc);
+    registerTransFunction(transpose_64_square_matrix_v6, trans_64_square_matrix_v6_desc);
 }
 
 /* 
