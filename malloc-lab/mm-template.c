8d588af9f792d4bcd4cfcf609ad54124a39fc33e
Soros Liu <soros.liu1029@gmail.com>
2020-10-13 13:06:43 +0800

    chore: add template for mm.c

---
+++ malloc-lab/mm-template.c
@@ -0,0 +1,56 @@
+/*
+ * mm-template.c
+ */
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "memlib.h"
+#include "mm.h"
+
+/*
+ * mm_init - Called when a new trace starts.
+ */
+int mm_init(void) {
+    return 0;
+}
+
+/*
+ * malloc - Allocate a block by incrementing the brk pointer.
+ *      Always allocate a block whose size is a multiple of the alignment.
+ */
+void *malloc(size_t size) {
+    return NULL;
+}
+
+/*
+ * free - We don't know how to free a block.  So we ignore this call.
+ *      Computers have big memories; surely it won't be a problem.
+ */
+void free(void *ptr) {
+}
+
+/*
+ * realloc - Change the size of the block by mallocing a new block,
+ *      copying its data, and freeing the old block.  I'm too lazy
+ *      to do better.
+ */
+void *realloc(void *oldptr, size_t size) {
+    return NULL;
+}
+
+/*
+ * calloc - Allocate the block and set it to zero.
+ */
+void *calloc(size_t nmemb, size_t size) {
+    return NULL;
+}
+
+/*
+ * mm_checkheap - There are no bugs in my code, so I don't need to check,
+ *      so nah!
+ */
+void mm_checkheap(int verbose) {
+}
--- malloc-lab/mm.h
+++ malloc-lab/mm.h
@@ -23,3 +23,31 @@ extern int mm_init(void);
 /* This is largely for debugging.  You can do what you want with the
    verbose flag; we don't care. */
 extern void mm_checkheap(int verbose);
+
+/* If you want debugging output, use the following macro.  When you hand
+ * in, remove the #define DEBUG line. */
+#define DEBUG
+#ifdef DEBUG
+#define dbg_printf(...) printf(__VA_ARGS__)
+#else
+#define dbg_printf(...)
+#endif
+
+/* do not change the following! */
+#ifdef DRIVER
+/* create aliases for driver tests */
+#define malloc mm_malloc
+#define free mm_free
+#define realloc mm_realloc
+#define calloc mm_calloc
+#endif /* def DRIVER */
+
+/* single word (4) or double word (8) alignment */
+#define ALIGNMENT 8
+
+/* rounds up to the nearest multiple of ALIGNMENT */
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
+
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+#define SIZE_PTR(p) ((size_t *)(((char *)(p)) - SIZE_T_SIZE))
